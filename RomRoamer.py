96ae47c6bab547bfd4ba7475a3fda8ea9e3979e0
Roberto <rob.uniud@gmail.com>
2019-05-07 17:03:42 +0200

    Added exception handling code

--- RomRoamer.py
+++ RomRoamer.py
@@ -3,39 +3,43 @@ from time import sleep
 
 """
 RomRoamer: automatically wander playing with 
-	   a GBC/GBA/NDS rom. (E.G. to hatch
+	   a GBC/GBA/NDS rom (E.G. to hatch
 	   eggs in Pokémon games or level up 
-	   a Pokémon left at the Day Care)
+	   a Pokémon left at the Day Care).
 """
 
 
 class RomRoamer(object):
 
-    def roam(times: int):
-        keyboard = Controller()
-        sleep(3)
-        for i in range(times):
+    def roam():
+        times = input("\nHow many times do you want to roam in a square? ")
+        try:
+            keyboard = Controller()
+            sleep(3)
+            for i in range(int(times)):
 
-            # Go down
-            keyboard.press(Key.down)
-            sleep(0.8)
-            keyboard.release(Key.down)
+                # Go down
+                keyboard.press(Key.down)
+                sleep(0.8)
+                keyboard.release(Key.down)
 
-            # Go right
-            keyboard.press(Key.right)
-            sleep(0.8)
-            keyboard.release(Key.right)
+                # Go right
+                keyboard.press(Key.right)
+                sleep(0.8)
+                keyboard.release(Key.right)
 
-            # Go up
-            keyboard.press(Key.up)
-            sleep(0.8)
-            keyboard.release(Key.up)
+                # Go up
+                keyboard.press(Key.up)
+                sleep(0.8)
+                keyboard.release(Key.up)
 
-            # Go left
-            keyboard.press(Key.left)
-            sleep(0.8)
-            keyboard.release(Key.left)
+                # Go left
+                keyboard.press(Key.left)
+                sleep(0.8)
+                keyboard.release(Key.left)
 
+        except ValueError as e:
+                msg = "Insert a valid number.\n{}: {}".format(type(e).__name__, e)
+                print(msg)
 
-    num = input("How many times do you want to roam in a square? ")
-    roam(int(num))
+    roam()
