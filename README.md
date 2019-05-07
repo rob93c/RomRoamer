d87e6e4fb5f8bb2417a6bb03e2f968336b109b52
Cella Roberto <rob.uniud@gmail.com>
2019-05-07 09:58:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+
+# License
+
+See the [LICENSE](https://github.com/rob93c/RomRoamer/blob/master/LICENSE.md) file for license rights and limitations (MIT).
---
+++ RomRoamer.py
@@ -0,0 +1,36 @@
+from pynput.keyboard import Key, Controller
+from time import sleep
+
+"""
+RomRoamer: automatically wander playing with 
+		   a GBC/GBA/NDS rom. (E.G. to hatch
+		   eggs in Pokémon games or level up 
+		   a Pokémon left at the Day Care)
+"""
+
+
+class RomRoamer(object):
+
+    def roam(times: int):
+        keyboard = Controller()
+        sleep(3)
+        for i in range(times + 1):
+            # Go down
+            keyboard.press(Key.down)
+            sleep(1)
+            keyboard.release(Key.down)
+            # Go right
+            keyboard.press(Key.right)
+            sleep(1)
+            keyboard.release(Key.right)
+            # Go up
+            keyboard.press(Key.up)
+            sleep(1)
+            keyboard.release(Key.up)
+            # Go left
+            keyboard.press(Key.left)
+            sleep(1)
+            keyboard.release(Key.left)
+
+    num = input("How many times do you want to roam in a square? ")
+    roam(int(num))
