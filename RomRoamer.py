f5c90beb4c30a29b6932e00e57b3daf066001306
Roberto <rob.uniud@gmail.com>
2019-05-11 12:05:16 +0200

    Error handling fixes

--- RomRoamer.py
+++ RomRoamer.py
@@ -15,40 +15,38 @@ RomRoamer: automatically wander playing with
 class RomRoamer:
 
     def __init__(self) -> None:
-        self.times = int(
-            input("\nHow many times do you want to roam in a square? "))
+        self.data = input("\nHow many times do you want to roam in a square? ")
+        if not self.data.isdigit():
+            raise ValueError("Insert a valid number.\n")
+        self.times = int(self.data)
 
-    def roam(self) -> None:
-        try:
-            keyboard = Controller()
-            sleep(3)
-            while self.times > 0:
+    def main(self) -> None:
+        keyboard = Controller()
+        sleep(3)
+        while self.times > 0:
 
-                # Go down
-                keyboard.press(Key.down)
-                sleep(0.8)
-                keyboard.release(Key.down)
+            # Go down
+            keyboard.press(Key.down)
+            sleep(0.8)
+            keyboard.release(Key.down)
 
-                # Go right
-                keyboard.press(Key.right)
-                sleep(0.8)
-                keyboard.release(Key.right)
+            # Go right
+            keyboard.press(Key.right)
+            sleep(0.8)
+            keyboard.release(Key.right)
 
-                # Go up
-                keyboard.press(Key.up)
-                sleep(0.8)
-                keyboard.release(Key.up)
+            # Go up
+            keyboard.press(Key.up)
+            sleep(0.8)
+            keyboard.release(Key.up)
 
-                # Go left
-                keyboard.press(Key.left)
-                sleep(0.8)
-                keyboard.release(Key.left)
+            # Go left
+            keyboard.press(Key.left)
+            sleep(0.8)
+            keyboard.release(Key.left)
 
-                self.times -= 1
+            self.times -= 1
 
-        except ValueError as e:
-            msg = "Insert a valid number.\n{}: {}".format(type(e).__name__, e)
-            print(msg)
 
 go = RomRoamer()
-go.roam()
+go.main()
